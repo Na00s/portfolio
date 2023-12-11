5e40a97fc4216b71221b9c2ff4e9782a76226c41
Ali <aj3238@columbia.edu>
2023-12-11 11:51:12 +0100

    add contact section

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Website loaded and ready!");
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
+
